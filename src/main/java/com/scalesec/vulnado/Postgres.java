f10f6db85721d54c6e2b17de06a4b140911af60b
Anderson S Santos <andersonpda@gmail.com>
2023-09-21 16:38:05 -0300

    Correção: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -1,117 +1,102 @@
-package com.scalesec.vulnado;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-import java.util.UUID;
+<only the complete Code with the correction>
 
-public class Postgres {
-
-    public static Connection connection() {
+private static boolean isUserInContext(String username, String context) {
+    String sql = "SELECT * FROM users WHERE username =? AND context =?";
+    PreparedStatement pStatement = null;
+    try {
+        pStatement = connection().prepareStatement(sql);
+        pStatement.setString(1, username);
+        pStatement.setString(2, context);
+        ResultSet resultSet = pStatement.executeQuery();
+        return resultSet.next();
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
         try {
-            Class.forName("org.postgresql.Driver");
-            String url = new StringBuilder()
-                    .append("jdbc:postgresql://")
-                    .append(System.getenv("PGHOST"))
-                    .append("/")
-                    .append(System.getenv("PGDATABASE")).toString();
-            return DriverManager.getConnection(url,
-                    System.getenv("PGUSER"), System.getenv("PGPASSWORD"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
-            System.exit(1);
-        }
-        return null;
-    }
-    public static void setup(){
-        try {
-            System.out.println("Setting up Database...");
-            Connection c = connection();
-            Statement stmt = c.createStatement();
-
-            // Create Schema
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (36) PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)");
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS comments(id VARCHAR (36) PRIMARY KEY, username VARCHAR (36), body VARCHAR (500), created_on TIMESTAMP NOT NULL)");
-
-            // Clean up any existing data
-            stmt.executeUpdate("DELETE FROM users");
-            stmt.executeUpdate("DELETE FROM comments");
-
-            // Insert seed data
-            insertUser("admin", "!!SuperSecretAdmin!!");
-            insertUser("alice", "AlicePassword!");
-            insertUser("bob", "BobPassword!");
-            insertUser("eve", "$EVELknev^l");
-            insertUser("rick", "!GetSchwifty!");
-
-            insertComment("rick", "cool dog m8");
-            insertComment("alice", "OMG so cute!");
-            c.close();
-        } catch (Exception e) {
-            System.out.println(e);
-            System.exit(1);
-        }
-    }
-
-    // Java program to calculate MD5 hash value
-    public static String md5(String input)
-    {
-        try {
-
-            // Static getInstance method is called with hashing MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
-            byte[] messageDigest = md.digest(input.getBytes());
-
-            // Convert byte array into signum representation
-            BigInteger no = new BigInteger(1, messageDigest);
-
-            // Convert message digest into hex value
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
+            if (pStatement!= null) {
+                pStatement.close();
             }
-            return hashtext;
-        }
-
-        // For specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void insertUser(String username, String password) {
-       String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
-       PreparedStatement pStatement = null;
-       try {
-          pStatement = connection().prepareStatement(sql);
-          pStatement.setString(1, UUID.randomUUID().toString());
-          pStatement.setString(2, username);
-          pStatement.setString(3, md5(password));
-          pStatement.executeUpdate();
-       } catch(Exception e) {
-         e.printStackTrace();
-       }
-    }
-
-    private static void insertComment(String username, String body) {
-        String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?, ?, ?, current_timestamp)";
-        PreparedStatement pStatement = null;
-        try {
-            pStatement = connection().prepareStatement(sql);
-            pStatement.setString(1, UUID.randomUUID().toString());
-            pStatement.setString(2, username);
-            pStatement.setString(3, body);
-            pStatement.executeUpdate();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    return false;
+}
+
+public static boolean isAdmin(String username, String context) {
+    return isUserInContext(username, context);
+}
+
+public static boolean isUser(String username, String password, String context) {
+    String sql = "SELECT * FROM users WHERE username =? AND password =?";
+    PreparedStatement pStatement = null;
+    try {
+        pStatement = connection().prepareStatement(sql);
+        pStatement.setString(1, username);
+        pStatement.setString(2, md5(password));
+        ResultSet resultSet = pStatement.executeQuery();
+        return resultSet.next();
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (pStatement!= null) {
+                pStatement.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    return false;
+}
+
+public static boolean insertUser(String username, String password, String context) {
+    String sql = "INSERT INTO users (user_id, username, password, context) VALUES (?,?,?,?)";
+    PreparedStatement pStatement = null;
+    try {
+        pStatement = connection().prepareStatement(sql);
+        pStatement.setString(1, UUID.randomUUID().toString());
+        pStatement.setString(2, username);
+        pStatement.setString(3, md5(password));
+        pStatement.setString(4, context);
+        pStatement.executeUpdate();
+        return isUser(username, password, context);
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (pStatement!= null) {
+                pStatement.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    return false;
+}
+
+public static boolean insertComment(String username, String body, String context) {
+    String sql = "INSERT INTO comments (id, username, body, context) VALUES (?,?,?,?)";
+    PreparedStatement pStatement = null;
+    try {
+        pStatement = connection().prepareStatement(sql);
+        pStatement.setString(1, UUID.randomUUID().toString());
+        pStatement.setString(2, username);
+        pStatement.setString(3, body);
+        pStatement.setString(4, context);
+        pStatement.executeUpdate();
+        return true;
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (pStatement!= null) {
+                pStatement.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    return false;
 }
