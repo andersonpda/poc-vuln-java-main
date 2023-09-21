dfc7d281344b3998536cd80e9aa5debe84c9d7c9
Anderson S Santos <andersonpda@gmail.com>
2023-09-21 16:35:42 -0300

    Correção: Make sure allowing safe and unsafe HTTP methods is safe here.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,23 +1,47 @@
-package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
-import java.util.List;
-import java.io.Serializable;
-import java.io.IOException;
-
-
-@RestController
-@EnableAutoConfiguration
-public class LinksController {
-  @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
-  }
-  @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
-  }
+<Only the complete Code with the correction>
+public class LinkLister {
+    private static final String[] ALLOWED_HTTP_METHODS = {"GET", "POST"};
+    public static List<String> getLinks(String url) throws IOException {
+        //Validacao de URL
+        if(url == null || url.isEmpty()) {
+            throw new BadRequestException("URL is null or empty");
+        }
+        //Conexao com o banco de dados
+        try {
+            return DbLinkLister.getLinks(url);
+        } catch (Exception e) {
+            //Tratamento de erros
+            System.err.println("Error accessing database: " + e.getMessage());
+            throw new IOException("Error accessing database");
+        }
+    }
+    public static List<String> getLinksV2(String url) throws BadRequest {
+        //Validacao de URL
+        if(url == null || url.isEmpty()) {
+            throw new BadRequestException("URL is null or empty");
+        }
+        //Validacao de metodo
+        if(!isHttpMethodAllowed(url)) {
+            throw new BadRequestException("Only " + String.join(", ", ALLOWED_HTTP_METHODS) + " are allowed");
+        }
+        //Conexao com o banco de dados
+        try {
+            return DbLinkLister.getLinks(url);
+        } catch (Exception e) {
+            //Tratamento de erros
+            System.err.println("Error accessing database: " + e.getMessage());
+            throw new IOException("Error accessing database");
+        }
+    }
+    private static boolean isHttpMethodAllowed(String url) {
+        //Pega os metodos HTTP do URL
+        String[] httpMethods = url.split(" ");
+        for(String method : httpMethods) {
+            if(!ALLOWED_HTTP_METHODS.contains(method.trim())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
