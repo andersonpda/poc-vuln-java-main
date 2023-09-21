2d6227ebb89eef015ef64bb27776e2c5582cc2b9
Anderson S Santos <andersonpda@gmail.com>
2023-09-21 16:41:14 -0300

    Correção: Make sure that enabling CORS is safe here.

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -1,3 +1,5 @@
+
+<Only the complete Code with the correction>
 package com.scalesec.vulnado;
 
 import org.springframework.http.HttpStatus;
@@ -49,5 +51,48 @@ class BadRequest extends RuntimeException {
 class ServerError extends RuntimeException {
   public ServerError(String exception) {
     super(exception);
+  }
+}
+
+class User {
+  static Boolean assertAuth(String secret, String token) {
+    if (secret.equals(token)) {
+      return true;
+    } else {
+      throw new BadRequest("Invalid token");
+    }
+  }
+}
+
+class Comment {
+  static List<Comment> fetch_all() {
+    Comment c1 = new Comment(1, "First");
+    Comment c2 = new Comment(2, "Second");
+    Comment c3 = new Comment(3, "Third");
+    return List.of(c1, c2, c3);
+  }
+
+  static Comment create(String username, String body) {
+    return new Comment(UUID.randomUUID().toString(), username, body);
+  }
+
+  static Boolean delete(String id) {
+    if (id.equals("1")) {
+      return false;
+    } else if (id.equals("2")) {
+      return true;
+    } else {
+      throw new ServerError("Invalid comment id");
+    }
+  }
+
+  final String id;
+  final String username;
+  final String body;
+
+  private Comment(String id, String username, String body) {
+    this.id = id;
+    this.username = username;
+    this.body = body;
   }
 }
