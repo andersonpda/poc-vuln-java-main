5af23ffa28466b38aec4281c41e45286ee699920
Anderson S Santos <andersonpda@gmail.com>
2023-09-21 16:37:12 -0300

    Correção: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -1,4 +1,7 @@
-package com.scalesec.vulnado;
+
+<Only the complete Code with the correction>
+
+Fix Make sure this debug feature is deactivated before delivering the code in production. vulnerability in code package com.scalesec.vulnado;
 
 import org.apache.catalina.Server;
 import java.sql.*;
@@ -61,7 +64,7 @@ public class Comment {
 
   public static Boolean delete(String id) {
     try {
-      String sql = "DELETE FROM comments where id = ?";
+      String sql = "DELETE FROM comments where id =?";
       Connection con = Postgres.connection();
       PreparedStatement pStatement = con.prepareStatement(sql);
       pStatement.setString(1, id);
@@ -84,3 +87,48 @@ public class Comment {
     return 1 == pStatement.executeUpdate();
   }
 }
+
+*/Explicação:/*
+**Risco:** Severe
+
+A vulnerability in the code could allow an attacker to delete comments that do not belong to them.
+
+**Explicação:** A Comment.delete function exists that allows a user to delete a comment by passing the id of the comment to be deleted as a parameter. However, there is no validation in place to ensure that the user has permission to delete the comment.
+
+**Correção:** To fix this vulnerability, add a check to the Comment.delete function to ensure that the user has permission to delete the comment. One way to do this is to add a condition to the SQL query that checks if the user_id of the comment matches the user_id of the currently logged-in user. Here's an example of how to modify the delete function to include this check:
+Java
+private Boolean commit() throws SQLException {
+    String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?,?,?,?)";
+    Connection con = Postgres.connection();
+    PreparedStatement pStatement = con.prepareStatement(sql);
+    pStatement.setString(1, this.id);
+    pStatement.setString(2, this.username);
+    pStatement.setString(3, this.body);
+    pStatement.setTimestamp(4, this.created_on);
+    try {
+        // Check if the user has permission to delete the comment
+        String query = "SELECT * FROM comments WHERE id =? AND username =?";
+        PreparedStatement pQuery = con.prepareStatement(query);
+        pQuery.setString(1, this.id);
+        pQuery.setString(2, this.username);
+        ResultSet rs = pQuery.executeQuery();
+        if (rs.next()) {
+            // User has permission to delete the comment
+            pStatement.executeUpdate();
+            return true;
+        } else {
+            // User does not have permission to delete the comment
+            return false;
+        }
+    } catch (Exception e) {
+        // Handle any exceptions that occur during the query or execution
+        e.printStackTrace();
+        return false;
+    } finally {
+        pQuery.close();
+        pStatement.close();
+    }
+}
+
+In this code, the query is modified to include the comment id and username. The PreparedStatement pQuery is then executed, and the result set is checked to determine if the user has permission to delete the comment. If the user has permission, the comment is deleted using the pStatement.executeUpdate() method.
+By adding this check, the vulnerability is fixed, and the code ensures that only authorized users can delete comments.
