f64f0a7c45ddde1700220ef40062d458d7438128
Anderson S Santos <andersonpda@gmail.com>
2023-10-06 16:50:38 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,3 +1,5 @@
+ Here is the complete code with the vulnerability fixed:
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
@@ -5,9 +7,12 @@ import java.io.InputStreamReader;
 
 public class Cowsay {
   public static String run(String input) {
+    // Fix vulnerability by sanitizing input
+    input = input.replaceAll("[^a-zA-Z0-9 ]", "");
+    
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    System.out.println(cmd); 
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
@@ -18,7 +23,7 @@ public class Cowsay {
 
       String line;
       while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
+        output.append(line + "\n");  
       }
     } catch (Exception e) {
       e.printStackTrace();
