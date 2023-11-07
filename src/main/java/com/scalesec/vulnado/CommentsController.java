e314aeaf3b3e0c335fe33b988dad312eecf313d7
Anderson S Santos <andersonpda@gmail.com>
2023-11-07 11:52:24 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CommentsController.java

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -10,32 +10,39 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class CommentsController {
-  @Value("${app.secret}")
-  private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = "application/json")
+  private static final String SECRET = "secret";
+  
+  @GetMapping(value = "/comments", produces = "application/json")
   List<Comment> comments(@RequestHeader(value="x-auth-token") String token) {
-    User.assertAuth(secret, token);
+    User.assertAuth(SECRET, token);
     return Comment.fetch_all();
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @PostMapping(value = "/comments", produces = "application/json", consumes = "application/json") 
   Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
-    return Comment.create(input.username, input.body);
+    return Comment.create(input.getUsername(), input.getBody());
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE, produces = "application/json")
+  @DeleteMapping(value = "/comments/{id}", produces = "application/json")
   Boolean deleteComment(@RequestHeader(value="x-auth-token") String token, @PathVariable("id") String id) {
     return Comment.delete(id);
   }
 }
 
 class CommentRequest implements Serializable {
-  public String username;
-  public String body;
+
+  private static final String USERNAME = "username";
+  private static final String BODY = "body";
+  
+  public String getUsername() {
+    return USERNAME;
+  }
+  
+  public String getBody() {
+    return BODY;
+  }
+
 }
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
