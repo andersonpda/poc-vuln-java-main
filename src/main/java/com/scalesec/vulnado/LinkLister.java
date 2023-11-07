003da891051ee79b406f623d9b6a33fd502dcc1c
Anderson S Santos <andersonpda@gmail.com>
2023-11-07 11:54:47 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinkLister.java

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -8,9 +8,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 import java.net.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LinkLister {
+  
+  private static final Logger logger = LoggerFactory.getLogger(LinkLister.class);
+  
+  private LinkLister() {
+    // private constructor 
+  }
+
   public static List<String> getLinks(String url) throws IOException {
     List<String> result = new ArrayList<String>();
     Document doc = Jsoup.connect(url).get();
@@ -25,7 +33,7 @@ public class LinkLister {
     try {
       URL aUrl= new URL(url);
       String host = aUrl.getHost();
-      System.out.println(host);
+      logger.info(host);
       if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
         throw new BadRequest("Use of Private IP");
       } else {
@@ -34,5 +42,31 @@ public class LinkLister {
     } catch(Exception e) {
       throw new BadRequest(e.getMessage());
     }
+  }
+  
+  // test methods
+  public static void main(String[] args) throws Exception {
+    testPrivateIP();
+    testPublicIP();
+  }
+
+  public static void testPrivateIP() throws Exception {
+    try {
+      getLinksV2("http://192.168.0.1");
+    } catch (BadRequest e) {
+      logger.info("Private IP blocked"); 
+    }
+  }
+  
+  public static void testPublicIP() throws Exception {
+    List<String> links = getLinksV2("http://example.com");
+    logger.info("Public IP allowed, links: " + links);
+  }
+
+}
+
+class BadRequest extends Exception {
+  public BadRequest(String message) {
+    super(message);
   }
 }
