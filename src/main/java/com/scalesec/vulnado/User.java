9fc38c0102d51a0a6ab21c7cce7ca8297ea9fc43
Anderson Sapucaia <anderson.dev@outlook.com>
2023-11-07 16:14:51 -0300

    Novo metodo aqui

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -61,4 +61,30 @@ public class User {
       return user;
     }
   }
+
+  public static User getById(String id) {
+    Statement stmt = null;
+    User user = null;
+    try {
+      Connection cxn = Postgres.connection();
+      stmt = cxn.createStatement();
+      System.out.println("Opened database successfully");
+
+      String query = "select * from users where id = " + id;
+      System.out.println(query);
+      ResultSet rs = stmt.executeQuery(query);
+      if (rs.next()) {
+        String user_id = rs.getString("user_id");
+        String username = rs.getString("username");
+        String password = rs.getString("password");
+        user = new User(user_id, username, password);
+      }
+      cxn.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName()+": "+e.getMessage());
+    } finally {
+      return user;
+    }
+  }
 }
