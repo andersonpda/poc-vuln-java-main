7a07c6a0abce33a45ed4c14b82a0537d523eabce
Anderson S Santos <andersonpda@gmail.com>
2023-11-08 19:06:28 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinkLister.java

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -4,13 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.IOException;
-import java.net.*;
-
+import java.util.logging.Logger;
 
 public class LinkLister {
+  private static final Logger logger = Logger.getLogger(LinkLister.class.getName());
+
+  private LinkLister() {
+    // private constructor
+  }
+
   public static List<String> getLinks(String url) throws IOException {
     List<String> result = new ArrayList<String>();
     Document doc = Jsoup.connect(url).get();
@@ -23,15 +30,15 @@ public class LinkLister {
 
   public static List<String> getLinksV2(String url) throws BadRequest {
     try {
-      URL aUrl= new URL(url);
+      URL aUrl = new URL(url);
       String host = aUrl.getHost();
-      System.out.println(host);
-      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
+      logger.info(host);
+      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")) {
         throw new BadRequest("Use of Private IP");
       } else {
         return getLinks(url);
       }
-    } catch(Exception e) {
+    } catch (Exception e) {
       throw new BadRequest(e.getMessage());
     }
   }
