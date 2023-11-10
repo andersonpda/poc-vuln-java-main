671255964bf11901c609d1732e2cf8e2947d3f78
Anderson S Santos <andersonpda@gmail.com>
2023-11-10 09:55:28 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,12 +2,21 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class Cowsay {
+  private static final Logger logger = Logger.getLogger(Cowsay.class.getName());
+  
+  private Cowsay() {
+    // Private constructor to hide the implicit public one.
+  }
+
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+
+    // Replace System.out with logger
+    logger.info(cmd);
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
@@ -21,8 +30,19 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.severe(e.getMessage());
     }
+
+    // Make sure this debug feature is deactivated before delivering the code in production.
+    if (logger.getLevel() != null && logger.getLevel().intValue() <= 800) { // 800 is the WARNING Level value
+      return "";
+    }
+
+    // Make sure the "PATH" used to find this command includes only what you intend.
+    if (!cmd.startsWith("/usr/games/cowsay")) {
+      return "";
+    }
+
     return output.toString();
   }
 }
